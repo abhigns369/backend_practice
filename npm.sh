bc1e726770849146cf395b048600e2cdcef62cf7
abhigns369 <abhignsp369@gmail.com>
2023-12-10 00:59:20 +0530

    setup success

---
+++ npm.sh
@@ -0,0 +1,21 @@
+npm init -y
+
+npm i -D prettier
+npm i -D nodemon
+npm i mongoose express dotenv
+mkdir public src
+touch .env .gitignore .prettierignore .prettierrc readme.md
+cd src
+mkdir controllers db middlewares models routes utils
+touch index.js app.js constants.js
+cd ..
+cd public
+mkdir temp
+cd temp
+touch .gitkeep
+cd ../..
+
+git init
+git add .
+git commit -m "setup success"
+git branch -M main
